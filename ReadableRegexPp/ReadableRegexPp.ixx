365d62a5e7cc2fe3f51cb4570f18f9b743c038ef
Eran-YT <eran.yt@icloud.com>
2023-03-02 09:41:43 +0200

    Implemeted character classes

--- ReadableRegexPp/ReadableRegexPp.ixx
+++ ReadableRegexPp/ReadableRegexPp.ixx
@@ -11,8 +11,38 @@ namespace rrpp
 		return ".";
 	}
 
+	// vvv Character classes vvv
+
 	export constexpr std::string digit()
 	{
 		return "\\d";
 	}
+
+	export constexpr std::string non_digit()
+	{
+		return "\\D";
+	}
+
+	export constexpr std::string whitespace()
+	{
+		return "\\s";
+	}
+	
+	export constexpr std::string non_whitespace()
+	{
+		return "\\S";
+	}
+
+	export constexpr std::string word()
+	{
+		return "\\w";
+	}
+
+	export constexpr std::string non_word()
+	{
+		return "\\W";
+	}
+
+	// ^^^ Character classes ^^^
+
 }
--- ReadableRegexPpTests/test.cpp
+++ ReadableRegexPpTests/test.cpp
@@ -14,11 +14,50 @@ TEST(TestReadableRegex, Digit) {
 	EXPECT_EQ(created_regex, "\\d");
 }
 
+TEST(TestReadableRegex, NonDigit) {
+	std::string created_regex = rrpp::non_digit();
+	EXPECT_EQ(created_regex, "\\D");
+}
+
+TEST(TestReadableRegex, Whitespace) {
+	std::string created_regex = rrpp::whitespace();
+	EXPECT_EQ(created_regex, "\\s");
+}
+
+TEST(TestReadableRegex, NonWhitespace) {
+	std::string created_regex = rrpp::non_whitespace();
+	EXPECT_EQ(created_regex, "\\S");
+}
+
+TEST(TestReadableRegex, Word) {
+	std::string created_regex = rrpp::word();
+	EXPECT_EQ(created_regex, "\\w");
+}
+
+TEST(TestReadableRegex, NonWord) {
+	std::string created_regex = rrpp::non_word();
+	EXPECT_EQ(created_regex, "\\W");
+}
+
 TEST(TestRunReadableRegex, Digit) {
 	std::regex created_regex(rrpp::digit());
 
 	EXPECT_TRUE(std::regex_match("1", created_regex));
 	EXPECT_FALSE(std::regex_match("Hello, World!", created_regex));
+}
+
+TEST(TestRunReadableRegex, Whitespace) {
+	std::regex created_regex(rrpp::whitespace());
+
+	EXPECT_TRUE(std::regex_match(" ", created_regex));
+	EXPECT_FALSE(std::regex_match("Hello", created_regex));
+}
+
+TEST(TestRunReadableRegex, Word) {
+	std::regex created_regex(rrpp::word());
+
+	EXPECT_TRUE(std::regex_match("H", created_regex));
+	EXPECT_FALSE(std::regex_match("123", created_regex));
 }
 
 TEST(TestReadableRegexComposition, Simple) {
